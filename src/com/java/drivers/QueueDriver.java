4630c56c174de035cbb94f170416e883e25a84ce
coolnits88 <coolnits88@gmail.com>
2015-01-10 00:59:14 +0530

    Added files

--- src/com/java/drivers/QueueDriver.java
+++ src/com/java/drivers/QueueDriver.java
@@ -1,6 +1,7 @@
 package com.java.drivers;
 
 import com.java.utils.UserExceptions.InvalidFunctionCallException;
+import com.java.utils.ds.LinkedQueue;
 import com.java.utils.ds.Queue;
 
 public class QueueDriver {
@@ -10,26 +11,26 @@ public class QueueDriver {
   }
 
   public static void main(String[] args) throws Exception {
-    Queue<Integer> s = new Queue<Integer>();
+    //Queue<Integer> s = new Queue<Integer>();
+    LinkedQueue<Integer> s = new LinkedQueue<Integer>();
+
+
+
     s.enqueue(1);
     s.enqueue(2);
-
     s.enqueue(3);
-    // System.out.println(" Capacity " + s.Capacity + " size " + s.size());
     s.enqueue(4);
     System.out.println(s.dequeue());
     System.out.println(s.dequeue());
-    // System.out.println(" Capacity " + s.Capacity + " size " + s.size());
     s.enqueue(5);
-    // System.out.println(" Main Thread First " + s.First + "Last " + s.Last);
     s.enqueue(6);
+    
     for (int si : s) {
       System.out.println("Iterator1: " + si);
     }
-
+    
     s.enqueue(7);
     s.enqueue(8);
-    // System.out.println(" Capacity " + s.Capacity + " size " + s.size());
     s.enqueue(9);
     System.out.println(s.dequeue());
     System.out.println(s.dequeue());
@@ -39,18 +40,16 @@ public class QueueDriver {
     s.enqueue(12);
     s.enqueue(13);
     s.enqueue(14);
+     // System.out.println(s.dequeue());
+    // System.out.println(s.dequeue());
+    // System.out.println(s.dequeue());
+    // System.out.println(s.dequeue());
+    // System.out.println(s.dequeue());
+    // System.out.println(s.dequeue());
+    // System.out.println(s.dequeue());
+    // System.out.println(s.dequeue());
     // System.out.println(" Capacity " + s.Capacity + " size " + s.size());
     // System.out.println(s.dequeue());
-    // System.out.println(s.dequeue());
-    // System.out.println(s.dequeue());
-    // System.out.println(s.dequeue());
-    // System.out.println(s.dequeue());
-    // System.out.println(s.dequeue());
-    // System.out.println(s.dequeue());
-    // System.out.println(s.dequeue());
-    // System.out.println(" Capacity " + s.Capacity + " size " + s.size());
-    // System.out.println(s.dequeue());
-    // System.out.println(" Capacity " + s.Capacity + " size " + s.size());
     for (int si : s) {
       System.out.println("Iterator2: " + si);
     }
